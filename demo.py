3b59e5229660fc37430441fb79394fb2f610906b
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-04-10 19:35:16 -0500

    Update demo.py
    warning! this code does not work!

--- demo.py
+++ demo.py
@@ -40,9 +40,10 @@ Additional thanks to Justin Engel.
 
 """
 
-from __future__ import division
+from __future__ import division,print_function
 
 import numpy
+import numpy as np
 import pyaudio
 import numba
 from matplotlib import mlab
@@ -51,11 +52,13 @@ from np_rw_buffer import RingBuffer
 from threading import Thread
 import math
 import time
+import array
 from time import sleep
 import dearpygui.dearpygui as dpg
 import snowy
 import matplotlib.cm as cm
-import array
+from scipy.interpolate import interp1d
+
 
 
 @numba.jit(numba.float64[:](numba.float64[:], numba.int32, numba.float64[:]), nopython=True, parallel=True, nogil=True, cache=True)
@@ -109,16 +112,149 @@ def shift3dximg(arr: list[numpy.float32], num: int, fill_value: list[numpy.float
     else:
         result[:] = arr
     return result
-
-
-
 #because numpy's zeroth array is the Y axis, we have to do this in the 1st dimension to shift the X axis
 #if the arrays are not the same size, don't attempt to use coordinates for fill value- it will fail.
 
 
 
-#@numba.jit(numba.float64[:])(numba.float64[:], numba.float64), nopython=True, parallel=True, nogil=True,cache=True)
-#def ITD(data: list[numpy.float64], timex: float) -> ( list[numpy.float64]):
+def detect_peaks(x, mph=None, mpd=1, threshold=0, edge='rising',
+                 kpsh=False, valley=False, show=False, ax=None):
+    """Detect peaks in data based on their amplitude and other features.
+    Parameters
+    ----------
+    x : 1D array_like
+        data.
+    mph : {None, number}, optional (default = None)
+        detect peaks that are greater than minimum peak height.
+    mpd : positive integer, optional (default = 1)
+        detect peaks that are at least separated by minimum peak distance (in
+        number of data).
+    threshold : positive number, optional (default = 0)
+        detect peaks (valleys) that are greater (smaller) than `threshold`
+        in relation to their immediate neighbors.
+    edge : {None, 'rising', 'falling', 'both'}, optional (default = 'rising')
+        for a flat peak, keep only the rising edge ('rising'), only the
+        falling edge ('falling'), both edges ('both'), or don't detect a
+        flat peak (None).
+    kpsh : bool, optional (default = False)
+        keep peaks with same height even if they are closer than `mpd`.
+    valley : bool, optional (default = False)
+        if True (1), detect valleys (local minima) instead of peaks.
+    show : bool, optional (default = False)
+        if True (1), plot data in matplotlib figure.
+    ax : a matplotlib.axes.Axes instance, optional (default = None).
+    Returns
+    -------
+    ind : 1D array_like
+        indeces of the peaks in `x`.
+    Notes
+    -----
+    The detection of valleys instead of peaks is performed internally by simply
+    negating the data: `ind_valleys = detect_peaks(-x)`
+
+    The function can handle NaN's 
+    See this IPython Notebook [1]_.
+    References
+    ----------
+    .. [1] http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
+    Examples
+    --------
+    """
+
+    x = np.atleast_1d(x).astype('float64')
+    if x.size < 3:
+        return np.array([], dtype=int)
+    if valley:
+        x = -x
+    # find indexes of all peaks
+    dx = x[1:] - x[:-1]
+    # handle NaN's
+    indnan = np.where(np.isnan(x))[0]
+    if indnan.size:
+        x[indnan] = np.inf
+        dx[np.where(np.isnan(dx))[0]] = np.inf
+    ine, ire, ife = np.array([[], [], []], dtype=int)
+    if not edge:
+        ine = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) > 0))[0]
+    else:
+        if edge.lower() in ['rising', 'both']:
+            ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
+        if edge.lower() in ['falling', 'both']:
+            ife = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) >= 0))[0]
+    ind = np.unique(np.hstack((ine, ire, ife)))
+    # handle NaN's
+    if ind.size and indnan.size:
+        # NaN's and values close to NaN's cannot be peaks
+        ind = ind[np.in1d(ind, np.unique(np.hstack((indnan, indnan - 1, indnan + 1))), invert=True)]
+    # first and last values of x cannot be peaks
+    if ind.size and ind[0] == 0:
+        ind = ind[1:]
+    if ind.size and ind[-1] == x.size - 1:
+        ind = ind[:-1]
+    # remove peaks < minimum peak height
+    if ind.size and mph is not None:
+        ind = ind[x[ind] >= mph]
+    # remove peaks - neighbors < threshold
+    if ind.size and threshold > 0:
+        dx = np.min(np.vstack([x[ind] - x[ind - 1], x[ind] - x[ind + 1]]), axis=0)
+        ind = np.delete(ind, np.where(dx < threshold)[0])
+    # detect small peaks closer than minimum peak distance
+    if ind.size and mpd > 1:
+        ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
+        idel = np.zeros(ind.size, dtype=bool)
+        for i in range(ind.size):
+            if not idel[i]:
+                # keep peaks with the same height if kpsh is True
+                idel = idel | (ind >= ind[i] - mpd) & (ind <= ind[i] + mpd) \
+                       & (x[ind[i]] > x[ind] if kpsh else True)
+                idel[i] = 0  # Keep current peak
+        # remove the small peaks and sort back the indexes by their occurrence
+        ind = np.sort(ind[~idel])
+
+    if show:
+        if indnan.size:
+            x[indnan] = np.nan
+        if valley:
+            x = -x
+        _plot(x, mph, mpd, threshold, edge, valley, ax, ind)
+
+    return ind
+
+
+def _plot(x, mph, mpd, threshold, edge, valley, ax, ind):
+    """Plot results of the detect_peaks function, see its help."""
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        print('matplotlib is not available.')
+    else:
+        if ax is None:
+            _, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+        ax.plot(x, 'b', lw=1)
+        if ind.size:
+            label = 'valley' if valley else 'peak'
+            label = label + 's' if ind.size > 1 else label
+            ax.plot(ind, x[ind], '+', mfc=None, mec='r', mew=2, ms=8,
+                    label='%d %s' % (ind.size, label))
+            ax.legend(loc='best', framealpha=.5, numpoints=1)
+        ax.set_xlim(-.02 * x.size, x.size * 1.02 - 1)
+        ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
+        yrange = ymax - ymin if ymax > ymin else 1
+        ax.set_ylim(ymin - 0.1 * yrange, ymax + 0.1 * yrange)
+        ax.set_xlabel('Data #', fontsize=14)
+        ax.set_ylabel('Amplitude', fontsize=14)
+        mode = 'Valley detection' if valley else 'Peak detection'
+        ax.set_title("%s (mph=%s, mpd=%d, threshold=%s, edge='%s')"
+                     % (mode, str(mph), mpd, str(threshold), edge))
+        # plt.grid()
+        plt.show()
+
+
+
+
+#@numba.jit(numba.float64[:](numba.float64[:]), nopython=True, parallel=True, nogil=True,cache=True)
+def ITD(data: list[numpy.float64]) -> ( list[numpy.float64]):
     # notes:
     # The pythonic way to COPY an array is to do x[:] = y[:]
     # do x=y and it wont copy it, so any changes made to X will also be made to Y.
@@ -132,10 +268,122 @@ def shift3dximg(arr: list[numpy.float32], num: int, fill_value: list[numpy.float
     # estimation and by simply changing all other code to iterate over 2d instead of 1d
     # care must be taken with numba parallelization/vectorization
 
-   # with numba.objmode(start=numba.float64):
-    #    start = time.time()
+    #we will now implement the intrinsic time-scale decomposition algorithm.
+    #function H=itd(x)
+    N_max = 10;
+    working_set = numpy.zeros_like(data)
+    working_set[:] = data [:]
 
-   # return data
+   # xx = working_set xx=x(:)'; this does numpy.concatanate and then something else idk
+    E_x = sum(working_set.dot())
+
+
+
+    N_max = 10
+    H = []
+    L1 = []
+    H1 = []
+    xx = working_set.transpose()
+    E_x = numpy.square(working_set) # same thing as E_x=sum(x.^2);
+    counter = 0
+    STOP = False
+    while 1:
+        counter = counter + 1
+        list [L1, H1] = itd_baseline_extract(xx)
+        numpy.append(H, H1)
+        STOP = stop_iter(xx, counter, N_max, E_x)
+        if STOP:
+            numpy.append(H, L1)
+            break
+        xx[:] = L1[:]
+
+    with numba.objmode(start=numba.float64):
+       start = time.time()
+
+    return data
+
+
+
+def stop_iter(xx,counter,N_max,E_x) -> (bool):
+    STOP = False
+    if (counter>N_max):
+        STOP=True
+    return
+
+    Exx=sum(xx.dot())
+    if (Exx<=0.01*E_x):
+        STOP=True
+        return
+    #https://blog.ytotech.com/2015/11/01/findpeaks-in-python/ we may want to switch
+    #to the PeakUtils interpolate function for better results
+    #however, since there is no filtering going on here, we will use Marcos Duarte's code
+    pks1= detect_peaks(xx)
+    pks2= detect_peaks(-xx)
+    pks= set.union(pks1,pks2)
+    if length((pks)<=7):
+        STOP=True
+        return
+    return
+
+def itd_baseline_extract(x: list[numpy.float64]) -> (list[numpy.float64], list[numpy.float64]):
+
+    working_set = numpy.zeros_like(x)
+    working_set[:] = numpy.transpose(x)
+    t = range(working_set.shape[0]) # t=1:length(x); should do the same as this
+
+
+    alpha=0.5;
+    [val_max, idx_max]= detect_peaks(x)
+    [val_min, idx_min]= detect_peaks(-x)
+    idx_cb=set.union(idx_max,idx_min)
+    val_min=-val_min
+    if (min(idx_max)<min(idx_min)):
+        idx_min=[idx_max[0],idx_min]
+        val_min= [val_min[0],val_min]
+    elif ( min(idx_max)> min(idx_min)):
+        idx_max=[idx_min[0],idx_max]
+        val_max=[val_max[0],val_max]
+
+    if (max(idx_max)>max(idx_min)):
+        idx_min=[idx_min,idx_max[-1]]
+        val_min=[val_min, val_min[-1]]
+    elif (max(idx_max)< max(idx_min)):
+        idx_max=[idx_max,idx_min[-1]]
+        val_max=[val_max, val_max[-1]]
+    H = numpy.zeros_like(x)
+    L = numpy.zeros_like(x)
+
+    #https://localcoder.org/why-does-matlab-interp1-produce-different-results-than-numpy-interp
+   # "interp1(x,v,xq) returns interpolated values of a 1-D function at specific query points using linear interpolation.
+    # Vector x contains the sample points, and v contains the corresponding values, v(x).
+    # Vector xq contains the coordinates of the query points."
+    Max_line=interp1d(idx_max,val_max,kind='linear')(t)
+    Min_line=interp1d(idx_min,val_min,kind='linear')(t)
+    Lk1=alpha*Max_line(idx_min)+val_min*(1-alpha)
+    Lk2=alpha*Min_line(idx_max)+val_max*(1-alpha)
+
+
+    Lk1=  numpy.vstack(idx_min, Lk1)  #Lk1=[idx_min(:),Lk1(:)];
+    #this line means create a matrix(an array in 2d) and stack each element in it's own row
+    Lk2=numpy.vstack([idx_max,*Lk2])
+    Lk=numpy.vstack([Lk1,*Lk2])
+    #https://www.mathworks.com/matlabcentral/answers/73735-what-does-it-mean-by-writing-idx-in-code
+    #~ means discard the first output of the sort function
+    #which means it only wants the VALUES to be in LK_col_2
+    Lk_col_2 = numpy.sort(Lk, axis= 0) # sort by first axis?
+    Lk_sorted=Lk[Lk_col_2,:]
+    Lk=Lk_sorted[1:-1,:]
+    #Lk=[[1,Lk(1,2)];Lk;[working_set.shape[0],Lk[:-1,2]]
+    idx_Xk=[1,idx_cb,working_set.shape[0]];
+    for i in range(len(idx_Xk)-1):
+        for j in range (idx_Xk[i],idx_Xk[i+1]):
+            kij=(Lk[i+1,2]-Lk[i,2])/(x(idx_Xk[i+1])-x(idx_Xk[i])) #$compute the slope K
+            L[j]=Lk[i,2]+kij*(x[j]-x(idx_Xk[i]))
+
+    H= [i for i in working_set if i not in L]
+    return L,H
+
+
 
 class FilterRun(Thread):
     def __init__(self, rb, pb, channels, processing_size, dtype,work,time,floor,iterations,clean,run):
