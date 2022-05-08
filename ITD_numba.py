b8edb2a31f5065e4972e3a9e0bb7c1c84030ca29
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-05-08 07:49:28 -0500

    Update ITD_numba.py

--- ITD_numba.py
+++ ITD_numba.py
@@ -68,68 +68,83 @@ def detect_peaks(x: list[float]):
     return numpy.unique(ind)
 
 @numba.jit(numba.types.Tuple((numba.float64[:],numba.float64[:],numba.int64))(numba.float64[:]))
-def itd_baseline_extract(data: list[numpy.float64]):
+def itd_baseline_extract(data: list[numpy.float64])-> Tuple[numpy.ndarray, numpy.ndarray]:
 
-    x = numpy.asarray(data,dtype=numpy.float64)
-    rotation = numpy.zeros(x.shape[0],dtype=numpy.float64)
+        x = numpy.asarray(data,dtype=numpy.float64)
+        rotation = numpy.zeros_like(x)
 
-    t =  x.shape[0] - 1
-    alpha=0.5
-    # signal.find_peaks_cwt(x, 1)
-    idx_max =numpy.asarray(detect_peaks(x))
-    idx_min= numpy.asarray(detect_peaks(-x))
-    shared = numpy.intersect1d(idx_max, idx_min)
-    if len(shared) != 0:
-        print("Danger! how can peaks and valleys be the same?")
-        return x,rotation, 0 #trend decomposition is broken
-
-    val_max = x[idx_max] #get peaks based on indexes
-    val_min = x[idx_min]
-    val_min= -val_min
+        alpha=0.5
+        # signal.find_peaks_cwt(x, 1)
+        idx_max =numpy.asarray(detect_peaks(x))
+        idx_min= numpy.asarray(detect_peaks(-x))
+        val_max = x[idx_max] #get peaks based on indexes
+        val_min = x[idx_min]
+        val_min= -val_min
     
-    num_extrema = len(val_max) + len(val_min)
-    if(num_extrema<3):
-        return x,rotation, 0 #trend decomposition is complete - there are no more knots to consider.
-    extremabuffersize = num_extrema + 2
-    extrema_indices = numpy.zeros(extremabuffersize, dtype=numpy.int64)
-    extrema_indices[1:-1] = numpy.sort(numpy.unique(numpy.hstack((idx_max,idx_min)))) 
-    extrema_indices[-1] = len(x) - 1
-    
-    baseline_knots = numpy.zeros(len(extrema_indices))
-    baseline_knots[0] = numpy.mean(x[:2])
-    baseline_knots[-1] = numpy.mean(x[-2:])
+        num_extrema = len(val_max) + len(val_min)
 
-    #j = extrema_indices, k = k, baseline_knots = B, x =  τ
-    for k in range(1, len(extrema_indices) - 1):
-        baseline_knots[k] = alpha * (x[extrema_indices[k - 1]] + \
-        (extrema_indices[k] - extrema_indices[k - 1]) / (extrema_indices[k + 1] - extrema_indices[k - 1]) * \
-        (x[extrema_indices[k + 1]] - x[extrema_indices[k - 1]])) + \
+        extremabuffersize = num_extrema + 2
+        extrema_indices = numpy.zeros(extremabuffersize, dtype=numpy.int64)
+        extrema_indices[1:-1] = numpy.sort(numpy.unique(numpy.hstack((idx_max,idx_min)))) 
+        extrema_indices[-1] = len(x) - 1
+    
+        baseline_knots = numpy.zeros(len(extrema_indices))
+        baseline_knots[0] = numpy.mean(x[:2])
+        baseline_knots[-1] = numpy.mean(x[-2:])
+        #also reflections possible, but should be treated with caution
+
+        #j = extrema_indices, k = k, baseline_knots = B, x =  τ
+        for k in range(1, len(extrema_indices) - 1):
+            baseline_knots[k] = alpha * (x[extrema_indices[k - 1]] + \
+            (extrema_indices[k] - extrema_indices[k - 1]) / (extrema_indices[k + 1] - extrema_indices[k - 1]) * \
+            (x[extrema_indices[k + 1]] - x[extrema_indices[k - 1]])) + \
                            alpha * x[extrema_indices[k]]
     
-    baseline_new = numpy.zeros_like(x)
-    #baseline = b^j+1, x = bj(previous baseline)
-    for k in range(0, len(extrema_indices) - 1):
-        baseline_new[extrema_indices[k]:extrema_indices[k + 1]] = baseline_knots[k]  + \
-       (baseline_knots[k + 1] - baseline_knots[k]) / (x[extrema_indices[k + 1]] - x[extrema_indices[k]]) * \
-       (x[extrema_indices[k]:extrema_indices[k + 1]] - x[extrema_indices[k]])
-    
-    rotation[:] = numpy.subtract(x, baseline_new)
+        baseline_new = numpy.zeros_like(x)
 
-    return baseline_new , rotation, 1
+        for k in range(0, len(extrema_indices) - 1):
+            baseline_new[extrema_indices[k]:extrema_indices[k + 1]] = baseline_knots[k]  + \
+            (baseline_knots[k + 1] - baseline_knots[k]) / (x[extrema_indices[k + 1]] - x[extrema_indices[k]]) * \
+            (x[extrema_indices[k]:extrema_indices[k + 1]] - x[extrema_indices[k]])
+    
+        rotation[:] = numpy.subtract(x, baseline_new)
+
+        return rotation[:] , baseline_new[:]
 
 @numba.jit(numba.float64[:,:](numba.float64[:]))
-def ITD(data: list[numpy.float64]):
-    Lx, Hx, cont = itd_baseline_extract(numpy.transpose(numpy.asarray(data,dtype=numpy.float64)))
-    H = numpy.zeros((22,len(data)),dtype=numpy.float64)
-    H[0,:] = Hx[:]
-    counter = 1
-    while cont:
-        Lx, Hx, cont = itd_baseline_extract(Lx)
-        H[counter,:] = Hx[:]
-        counter = counter + 1
-        if counter > 20:
-            counter = counter - 1 
-            break#we've gone on long enough
-    counter = counter + 1
-    H[counter,:] = Lx[:]
-    return H[0:counter+1,:]
+def itd(self, data: numpy.ndarray, max_iteration: int = 22) -> numpy.ndarray:
+        rotations = numpy.zeros((max_iteration+1,len(data)),dtype=numpy.float64)
+        baselines = numpy.zeros((max_iteration+1,len(data)),dtype=numpy.float64)
+        rotation_ = numpy.zeros((len(data)),dtype=numpy.float64)
+        baseline_ = numpy.zeros((len(data)),dtype=numpy.float64)
+        r = numpy.zeros((len(data)),dtype=numpy.float64)
+        rotation_[:], baseline_[:] = itd_baseline_extract(numpy.transpose(numpy.asarray(data,dtype=numpy.float64))) 
+        counter = 0
+        while not finished:         
+            idx_max = numpy.asarray(detect_peaks(baseline_))
+            idx_min = numpy.asarray(detect_peaks(-baseline_))
+            num_extrema = len(idx_min) + len(idx_max)
+            print(num_extrema)
+            if num_extrema < 2:
+                #is the new baseline decomposable?
+                print("No more decompositions possible")
+                #implied: last decomposition was invalid!
+                #not always the case, but efforts to decompose the trend which are meaningful
+                #require a little adjustment to get the baseline monotonic trend to show properly.
+                r[:] = baselines[counter-1,:]
+                rotations[counter,:] = r[:]
+                counter = counter + 1      
+                return  rotations[0:counter,:]
+
+            elif counter > max_iteration:
+                print("Out of time!")
+                r[:] = numpy.add(rotation_[:],baseline_[:])
+                rotations[counter,:] = r[:]
+                counter = counter + 1 
+                return rotations[0:counter,:]
+
+            else: #results are sane, so perform an extraction.
+                rotations[counter,:] = rotation_[:]
+                baselines[counter,:] =  baseline_[:]
+                rotation_[:],  baseline_[:] = itd_baseline_extract(baseline_[:])
+                counter = counter + 1   
