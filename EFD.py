94466e1590e8937a562f81017130dea3605719bb
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-05-10 23:29:01 -0500

    Update EFD.py

--- EFD.py
+++ EFD.py
@@ -1,10 +1,4 @@
-#Author: falseywinchnet, shamelessly copied from matlab code and adapted for numpy.
-#warning : this code does not generate good output yet. Further validation is needed.
-
-
-
-
-
+#author: falsywinchnet, shameless stealer and modifier of matlab code
 def segm_tec(f,N):
 # 1. detect the local maxima and minina
     locmax = numpy.zeros_like(f);
@@ -15,7 +9,7 @@ def segm_tec(f,N):
             locmax[i]=f[i]
         if ((f[i-1]>f[i]) and (f[i]<f[i+1])):
             locmin[i]=f[i]
-    locmax[1] = f[1];
+    locmax[0] = f[0];
     locmax[-1] = f[-1];
     
                   
@@ -29,10 +23,10 @@ def segm_tec(f,N):
             desc_sort_index = numpy.sort(desc_sort_index)
             N = len(desc_sort)
         M = N+1# numbers of the boundaries
-        omega = numpy.concatenate(([1],desc_sort_index))
-        omega = numpy.concatenate((desc_sort_index,[len(f)]))
+        omega = numpy.concatenate(([0],desc_sort_index))
+        omega = numpy.concatenate((omega,[len(f)]))
         bounds = numpy.zeros((M))
-        for i in range(M-1):
+        for i in range(M):
             if (i == 0 or i == M) and (omega[i] == omega[i+1]):
                 bounds[i] = omega[i]-1;
             else:
@@ -45,13 +39,14 @@ def segm_tec(f,N):
 #https://arxiv.org/pdf/2009.08047v2.pdf
 def EFD(x: list[numpy.float64], N: int):
     #we will now implement the Empirical Fourier Decomposition
+    #what is it with me and signal decomposition approaches??
 
     #we will assume that x is 1d, if x is 2d, test and transform to put rows-first
     ff = numpy.fft.fft(x)
 
     #extract the boundaries of Fourier segments
     bounds,cerf = segm_tec(abs(ff[0:round(ff.size/2)]),N)
-
+    bounds[0] = 0 #fix : 5/10/22 temp patch for the first value
     # truncate the boundaries to [0,pi]
     bounds = bounds*numpy.pi/round(len(ff)/2)
     
@@ -59,28 +54,30 @@ def EFD(x: list[numpy.float64], N: int):
     # extend the signal by miroring to deal with the boundaries
     l = round(len(x)/2)
     #x = [x(l-1:-1:1);x;x(end:-1:end-l+1)];
-    z = numpy.concatenate((numpy.flip(x[0:l-1]),x))
-    z = numpy.concatenate((z,numpy.flip(x[l+1:])))
+    z = numpy.concatenate((numpy.flip(x[:l]),x))
+    z = numpy.concatenate((z,numpy.flip(x[-l:])))
     ff = numpy.fft.fft(z)
 
     # obtain the boundaries in the extend f
     bound2 = numpy.ceil(bounds*round(len(ff)/2)/numpy.pi).astype(dtype=int)
     print(bound2)
-    efd = numpy.zeros(((len(bound2)-1,len(x))))
+    efd = numpy.zeros(((len(bound2)-1,len(x))),dtype=numpy.float64)
     ft = numpy.zeros((efd.shape[0],len(ff)),dtype=numpy.cdouble)
     
     # define an ideal functions and extract components
     for k in range(efd.shape[0]): 
         if bound2[k] == 0:
-            ft[k,1:bound2[k+1]] = ff[1:bound2[k+1]]
+            ft[k,0:bound2[k+1]] = ff[0:bound2[k+1]]
             ft[k,len(ff)+2-bound2[k+1]:len(ff)] = ff[len(ff)+2-bound2[k+1]:len(ff)]
         else:
             ft[k,bound2[k]:bound2[k+1]] = ff[bound2[k]:bound2[k+1]]
             ft[k,len(ff)+2-bound2[k+1]:len(ff)+2-bound2[k]] = ff[len(ff)+2-bound2[k+1]:len(ff)+2-bound2[k]]
         rx = numpy.real(numpy.fft.ifft(ft[k,:]))
-        efd[k,:] = rx[l:-l+2]
+        efd[k,:] = rx[l:-l]
 
 
     return efd,cerf,bounds
 
+#for tomorrow: got the diff down considerably by fixing the omega first value to 0.
+#got the diff down further by fixing the array mirroring to only add L exactly
 
