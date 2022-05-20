ddbd5f5bc17b73fc6b18ad740dd14d0896f4b5d5
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-05-19 21:52:12 -0500

    Update EFD.py

--- EFD.py
+++ EFD.py
@@ -12,40 +12,39 @@ def segm_tec(f,N):
         if ((f[i-1] <= f[i]) and (f[i] > f[i+1])):
             if(f[i] > 0): #a value below 0 or 0 is not a maxima!
                 locmax[i]= 1
-        
-    #locmax[0] = 1
-    #locmax[-1] = 1
+
     
     #ok, now we have our maxima
     desc_sort_index = numpy.where(locmax == 1)[0]
 
-    ind = numpy.where(locmax == 1)[0]
+    indexes = numpy.where(locmax == 1)[0]
     top_amplitudes = numpy.argsort(f) #sort F by amplitude
     top_amplitudes = top_amplitudes[::-1] #this returns a reverse-sorted array.
-    desc_sort_bool = numpy.isin(top_amplitudes,ind)# get the top amplitudes which are peaks
+    desc_sort_bool = numpy.isin(top_amplitudes,indexes)# get the top amplitudes which are peaks
     
     desc_sort_index = top_amplitudes[desc_sort_bool] #retrieve them
     if N != 0: #keep the N-th highest maxima and their index
-        
         if len(desc_sort_index) > N:
-            desc_sort_index = desc_sort_index[0:N]
+            desc_sort_index = desc_sort_index[0:N+1]
         else:
-            desc_sort_index = desc_sort_index
-            N = len(desc_sort_index)
+            N = desc_sort_index.size
         desc_sort_index = numpy.sort(desc_sort_index) #gotta sort them again 
-        M = N+1# numbers of the boundaries
-        omega = numpy.concatenate(([0],desc_sort_index))
-        omega = numpy.concatenate((omega,[len(f)]))
-        bounds = numpy.zeros((M),dtype=int)
-        for i in range(M):
-            if (i == 0 or i == M) and (omega[i] == omega[i+1]):
-                bounds[i] = omega[i]-1;
-            else:
-                ind = numpy.argmin(f[omega[i]:omega[i+1]])
-                bounds[i] = omega[i]+ind-2;
+        bounds = []
+        bounds.append(0)
+        bounds.append(numpy.argmin(f[0:desc_sort_index[0]])) # -2
+        for i in range(1,N - 1):
+            bounds.append(desc_sort_index[i-1] + numpy.argmin(f[desc_sort_index[i-1]:desc_sort_index[i]]) - 1)     
+        bounds.append(desc_sort_index[N] + numpy.argmin(f[desc_sort_index[N]:len(f)]) - 1)
+        bounds.append(len(f))
+        bounds = numpy.asarray(bounds)
         cerf = desc_sort_index*numpy.pi/round(len(f))
     return bounds, cerf
-
+#[  0   5  11  18  24  30  33  40  45  52  57  60  93 793]
+#[  0   5  11  18  24  30  33  40  45  52  57  60  93 793]
+#[  1   9  14  21  25  30  36  40  47  52  58  61 773 793]
+#[  0   5  11  18  24  30  33  40  45  52  57  60  93 793]
+#[  0   5  11  18  24  30  33  40  45  52  57  60  93 793]
+#[  0 1   9  14  21  25  30  36  40  47  52  58  0]
 
 #https://arxiv.org/pdf/2009.08047v2.pdf
 def EFD(x: list[numpy.float64], N: int):
@@ -56,11 +55,8 @@ def EFD(x: list[numpy.float64], N: int):
     
     fx =  planfftw.fft(x)
     ff = fx(x)
-    fr = round((ff.size/2))
     #extract the boundaries of Fourier segments
     bounds,cerf = segm_tec(abs(ff[0:round(ff.size/2)]),N)
-    bounds = numpy.concatenate(([0],bounds)) #fix : 5/10/22 temp patch for the first value
-    bounds = numpy.concatenate((bounds,[fr])) #fix : 5/10/22 temp patch for the first value
     # truncate the boundaries to [0,pi]
     bounds = bounds*numpy.pi/round(len(ff)/2)
     
@@ -84,10 +80,10 @@ def EFD(x: list[numpy.float64], N: int):
     for k in range(efd.shape[0]): 
         if bound2[k] == 0:
             ft[k,0:bound2[k+1]] = ff[0:bound2[k+1]]
-            ft[k,len(ff)+2-bound2[k+1]:len(ff)] = ff[len(ff)+2-bound2[k+1]:len(ff)]
+            ft[k,len(ff)+1-bound2[k+1]:len(ff)] = ff[len(ff)+1-bound2[k+1]:len(ff)]
         else:
             ft[k,bound2[k]:bound2[k+1]] = ff[bound2[k]:bound2[k+1]]
-            ft[k,len(ff)+2-bound2[k+1]:len(ff)+2-bound2[k]] = ff[len(ff)+2-bound2[k+1]:len(ff)+2-bound2[k]]
+            ft[k,len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]] = ff[len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]]
         rx = numpy.real(fz(ft[k,:]))
         efd[k,:] = rx[l:-l]
 
