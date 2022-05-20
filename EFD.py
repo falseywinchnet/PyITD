7121199292e243a72e8e24826664173a644ab57b
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-05-20 18:44:09 -0500

    Update EFD.py

--- EFD.py
+++ EFD.py
@@ -19,24 +19,31 @@ def segm_tec(f, N):
 
     if N != 0:  # keep the N-th highest maxima and their index
         if len(desc_sort_index) > N:
-            desc_sort_index = desc_sort_index[0:N + 1]
+            desc_sort_index = desc_sort_index[0:N]
+            print(desc_sort_index)
+
         else:
             N = desc_sort_index.size
         desc_sort_index = numpy.sort(desc_sort_index)  # gotta sort them again
-        bounds = numpy.empty(N+2, dtype=int)
+        print(desc_sort_index)
+        bounds = numpy.zeros(N+2, dtype=int)
         bounds[0] = 0
-        bounds[1] = (numpy.argmin(f[0:desc_sort_index[0]]))  # -2
-        for i in range(N - 2):
-            bounds[i+2] = (desc_sort_index[i] + numpy.argmin(f[desc_sort_index[i]:desc_sort_index[i+1]]) - 1)
-        bounds[-2] = (desc_sort_index[N] + numpy.argmin(f[desc_sort_index[N]:len(f)]) - 1)
+        for i in range(N-1): #2-12 for 10 N
+            bounds[i+1] = (desc_sort_index[i] + numpy.argmin(f[desc_sort_index[i]:desc_sort_index[i+1]])) 
+        bounds[-2] = (desc_sort_index[-1] + numpy.argmin(f[desc_sort_index[-1]:len(f)])) 
         bounds[-1] = f.size
+
+
+        print(bounds)
         cerf = desc_sort_index * numpy.pi / round(len(f))
     return numpy.asarray(bounds), cerf
+
 
 #https://arxiv.org/pdf/2009.08047v2.pdf
 def EFD(x: list[numpy.float64], N: int):
     #we will now implement the Empirical Fourier Decomposition
     x = numpy.asarray(x,dtype=numpy.float64)
+    
     ff = numpy.fft.rfft(x)
     #extract the boundaries of Fourier segments
     bounds,cerf = segm_tec(abs(ff[0:round(ff.size/2)]),N)
@@ -45,8 +52,12 @@ def EFD(x: list[numpy.float64], N: int):
     
     # extend the signal by miroring to deal with the boundaries
     l = round(len(x)/2)
-    z = numpy.lib.pad(x,((round(len(x)/2)),round(len(x)/2)),'symmetric') 
-    ff =  numpy.fft.rfft(z)
+    z = numpy.lib.pad(x,((round(len(x)/2)),round(len(x)/2)),'symmetric')
+    
+    ff = numpy.fft.rfft(z)
+    #ff = out[:]
+            
+    #ff =  numpy.fft.rfft(z)
     # obtain the boundaries in the extend f
     bound2 = numpy.ceil(bounds*round(len(ff)/2)/numpy.pi).astype(dtype=int)
     efd = numpy.zeros(((len(bound2)-1,len(x))),dtype=numpy.float64)
@@ -55,11 +66,16 @@ def EFD(x: list[numpy.float64], N: int):
     for k in range(efd.shape[0]): 
         if bound2[k] == 0:
             ft[k,0:bound2[k+1]] = ff[0:bound2[k+1]]
+            #ft[k,len(ff)+1-bound2[k+1]:len(ff)] = ff[len(ff)+1-bound2[k+1]:len(ff)]
             ft[k,-bound2[k+1]:len(ff)] = ff[-bound2[k+1]:len(ff)]
+
         else:
             ft[k,bound2[k]:bound2[k+1]] = ff[bound2[k]:bound2[k+1]]
+            #ft[k,len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]] = ff[len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]]
+
             ft[k,-bound2[k+1]:-bound2[k]] = ff[-bound2[k+1]:-bound2[k]]
-        rx = numpy.fft.irfft(ft[k,:])
+        rx =  numpy.fft.irfft(ft[k,:]) 
         efd[k,:] = rx[l:-l]
+
 
     return efd,cerf,bounds
