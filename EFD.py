d55af8025ddacfab93dcc5a6fc95386352bc7ab7
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-06-09 01:11:48 -0500

    Update EFD.py

--- EFD.py
+++ EFD.py
@@ -1,74 +1,88 @@
 import numpy
+import pyfftw #optional, improves performance
+import planfftw
 #copied from the matlab by falsy winchnet.
 #Emperical Fourier Decomposition technique
 
 def segm_tec(f, N):
-    locmax = numpy.zeros((f.size))
-    for i in range(1, len(f) - 1):
-        if ((f[i - 1] <= f[i]) and (f[i] > f[i + 1])):
-            if (f[i] > 0):  # a value below 0 or 0 is not a maxima!
-                locmax[i] = f[i]
+    x = numpy.asarray(f)
+    dx = x[1:] - x[:-1]
+    # handle NaN's
+    indnan = numpy.where(numpy.isnan(x))[0]
+    indl = numpy.asarray(indnan)
 
+    if indl.size != 0:
+        x[indnan] = numpy.inf
+        dx[numpy.where(numpy.isnan(dx))[0]] = numpy.inf
+
+    vil = numpy.zeros(dx.size + 1)
+    vil[:-1] = dx[:]
+    vix = numpy.zeros(dx.size + 1)
+    vix[1:] = dx[:]
+
+    ind = numpy.unique(numpy.where((vil > 0) & (vix <= 0))[0])
+    if ind.size < 2:
+        return numpy.asarray([0,len(f)])
+    locmax = numpy.zeros((f.size),dtype=numpy.float64)
+    locmax[ind] = f[ind]
+    locmax[0] = 0
+    locmax[-1] = 0 #ends cannot be peaks!
     desc_sort_index = numpy.argsort(locmax)[::-1]
-    desc_sort_bool = numpy.empty(desc_sort_index.shape[0], dtype=bool)
-    for i in range(desc_sort_index.size):
-        if locmax[i] > 0:
-            desc_sort_bool[i] = True
-
-    desc_sort_index = desc_sort_index[desc_sort_bool]
-
+    desc_sort_index = desc_sort_index[0:ind.size]
     if N != 0:  # keep the N-th highest maxima and their index
         if len(desc_sort_index) > N:
             desc_sort_index = desc_sort_index[0:N + 1]
         else:
             N = desc_sort_index.size
         desc_sort_index = numpy.sort(desc_sort_index)  # gotta sort them again
-        bounds = numpy.empty(N+2, dtype=int)
-        bounds[0] = 0
+        bounds = numpy.zeros((N + 2), dtype=numpy.int64)
         bounds[1] = (numpy.argmin(f[0:desc_sort_index[0]]))  # -2
         for i in range(N - 2):
-            bounds[i+2] = (desc_sort_index[i] + numpy.argmin(f[desc_sort_index[i]:desc_sort_index[i+1]]) - 1)
+            bounds[i + 2] = (desc_sort_index[i] + numpy.argmin(f[desc_sort_index[i]:desc_sort_index[i + 1]]) - 1)
         bounds[-2] = (desc_sort_index[N] + numpy.argmin(f[desc_sort_index[N]:len(f)]) - 1)
         bounds[-1] = f.size
-        cerf = desc_sort_index * numpy.pi / round(len(f))
     return numpy.asarray(bounds), cerf
 
 
-#https://arxiv.org/pdf/2009.08047v2.pdf
-def EFD(x: list[numpy.float64], N: int):
-    #we will now implement the Empirical Fourier Decomposition
-    x = numpy.asarray(x,dtype=numpy.float64)
-    
-    ff = numpy.fft.fft(x)
-    #extract the boundaries of Fourier segments
-    bounds,cerf = segm_tec(abs(ff[0:round(ff.size/2)]),N)
-    print(bounds)
+def EFD(data: numpy.ndarray, N: int):
+    if N < 1:
+        return x
+
+    # we will now implement the Empirical Fourier Decomposition
+    x = numpy.asarray(data, dtype=numpy.float64)
+    fa = planfftw.rfft(x.shape)
+    ff = fa(x)
+    # extract the boundaries of Fourier segments
+    #with numba.objmode(out='int[:]'):
+    bounds = segm_tec(numpy.absolute(ff[0:round(ff.size / 2)]), N)
+    if bounds.size < 3:
+        return x #no need to go further because we have nothing to work with
     # truncate the boundaries to [0,pi]
-    bounds = bounds*numpy.pi/round(len(ff)/2)
-    
+    bounds = bounds * numpy.pi / round(len(ff) / 2)
+
     # extend the signal by miroring to deal with the boundaries
-    l = round(len(x)/2)
-    z = numpy.lib.pad(x,((round(len(x)/2)),round(len(x)/2)),'symmetric')
-    
-    ff = numpy.fft.fft(z)
+    l = round(len(x) / 2)
+    z = numpy.lib.pad(x, ((round(len(x) / 2)), round(len(x) / 2)), 'symmetric')
+    fz = planfftw.rfft(z.shape)
+
+    ff = fz(z)
     # obtain the boundaries in the extend f
-    bound2 = numpy.ceil(bounds*round(len(ff)/2)/numpy.pi).astype(dtype=int)
-    efd = numpy.zeros(((len(bound2)-1,len(x))),dtype=numpy.float64)
-    ft = numpy.zeros((efd.shape[0],len(ff)),dtype=numpy.cdouble)
+    bound2 = numpy.ceil(bounds * round(len(ff) / 2) / numpy.pi).astype(dtype=numpy.int64)
+    efd = numpy.zeros(((len(bound2) - 1, len(x))), dtype=numpy.float64)
+    ft = numpy.zeros((efd.shape[0], len(ff)), dtype=numpy.cdouble)
+    fx = planfftw.irfft(len(ff))
     # define an ideal functions and extract components
-    for k in range(efd.shape[0]): 
+    for k in range(efd.shape[0]):
         if bound2[k] == 0:
-            ft[k,0:bound2[k+1]] = ff[0:bound2[k+1]]
-            #ft[k,len(ff)+1-bound2[k+1]:len(ff)] = ff[len(ff)+1-bound2[k+1]:len(ff)]
-            ft[k,-bound2[k+1]:len(ff)] = ff[-bound2[k+1]:len(ff)]
+            ft[k, 0:bound2[k + 1]] = ff[0:bound2[k + 1]]
+            # ft[k,len(ff)+1-bound2[k+1]:len(ff)] = ff[len(ff)+1-bound2[k+1]:len(ff)]
+            ft[k, -bound2[k + 1]:len(ff)] = ff[-bound2[k + 1]:len(ff)]
 
         else:
-            ft[k,bound2[k]:bound2[k+1]] = ff[bound2[k]:bound2[k+1]]
-            #ft[k,len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]] = ff[len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]]
+            ft[k, bound2[k]:bound2[k + 1]] = ff[bound2[k]:bound2[k + 1]]
+            # ft[k,len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]] = ff[len(ff)+1-bound2[k+1]:len(ff)+1-bound2[k]]
+            ft[k, -bound2[k + 1]:-bound2[k]] = ff[-bound2[k + 1]:-bound2[k]]
+        rx = fx(ft[k, :])
+        efd[k, :] = rx[l:-l].real
 
-            ft[k,-bound2[k+1]:-bound2[k]] = ff[-bound2[k+1]:-bound2[k]]
-        rx =  numpy.real(numpy.fft.ifft(ft[k,:]))
-        efd[k,:] = rx[l:-l]
-
-
-    return efd,cerf,bounds
+    return efd.astype(dtype=data.dtype),cerf,bounds
