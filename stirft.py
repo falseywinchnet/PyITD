fbcd20cde19c4de90bca33dfc1f186073f6816e7
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2024-02-07 00:30:46 -0500

    Update stirft.py

--- stirft.py
+++ stirft.py
@@ -35,3 +35,85 @@ def compute_synthesis_window(analysis_window, hop):
         n += hop
 
     return analysis_window / norm
+
+
+
+import numpy as np
+def stirft(x:np.ndarray, window:np.ndarray) -> np.ndarray:
+    
+    n_fft = 512 #also the segment length
+    win_length = 512
+    hop_len = 128    
+      
+
+    xp = np.zeros(x.size+n_fft-1,dtype=np.float64)
+    xp[(hop_len*2):-(hop_len*2-1)]= x[:]
+    xp[0:(hop_len*2)] = xp[(hop_len*2+1):((hop_len*2)*2)+1][::-1]
+    xp[-(hop_len*2-1):] = xp[-(hop_len*2-1)*2-1:-(hop_len*2)][::-1]
+    
+    #Calculate parameters
+    n_overlap = n_fft - hop_len
+    n_segs = (xp.shape[-1] - n_fft) // hop_len + 1
+    s20 = int(np.ceil(n_fft / 2))
+    s21 = s20 - 1 if (n_fft % 2 == 1) else s20
+
+    # Segmentation
+    Sx = np.zeros((n_fft, n_segs), dtype=np.float64)
+    strides = (xp.strides[0], hop_len * xp.strides[0])
+
+    starts = np.arange(n_segs) * hop_len
+    first_half = np.lib.stride_tricks.as_strided(xp, (s21, n_segs), strides)
+    second_half = np.lib.stride_tricks.as_strided(xp[s21:], (s20, n_segs), strides)
+
+    Sx[:s20, :] = first_half
+    Sx[s20:, :] = second_half
+    #note: for dft centering on stft, we transpose the first and second half
+    #additionally, we perform fftshift on the window
+    #however, for the stirft we do not perform this
+
+
+
+    Sx *= window.reshape(-1, 1) #apply windowing
+    Zx = np.zeros((n_fft,n_segs),dtype=np.float64)
+
+    Zx[:] = np.fft.irfft(Sx, axis=0)[:n_fft]
+
+    return Zx
+def istirft(Sx: np.ndarray,persistent_buffer:np.ndarray,window:np.ndarray):
+
+
+    #persistent buffer is 384 samples here
+    n_fft = 512
+    win_len = 512
+    hop_len = 128
+    N = Sx.shape[1]*hop_len
+
+    Xout = np.zeros((n_fft*2-2,Sx.shape[1]),dtype=Sx.dtype)
+    Xout[:n_fft,:] = Sx[:]
+    Xout[n_fft:,:]=numpy.flip(Sx[1:n_fft-1,:],axis=0)
+
+    xbuf = np.fft.rfft(Sx, n=n_fft*2-2, axis=0).real
+    #perform fftshift here along first axis if dft centered stft
+
+    x = np.zeros(N, dtype=np.float64) 
+
+    n = 0
+    for i in range(xbuf.shape[1]):
+        processing = xbuf[:, i] * window
+        out = processing[0 : hop_len]  # fresh output samples
+        out[:128] += persistent_buffer[:128]
+        persistent_buffer[: -hop_len] = persistent_buffer[hop_len:]  # shift out left
+        persistent_buffer[-hop_len :] = 0.0
+        persistent_buffer[:] += processing[-384:] #n_FFT - hop_length
+        x[n : n + hop_len] = out[:]
+        n += hop_len
+    return x, persistent_buffer
+
+
+msewin= compute_synthesis_window(np.hanning(512),128) 
+s = stirft(x=data[0:rate],window=msewin)#note the use of the MSE window for the forward transform, this is intentional
+i,buf = istirft(s,numpy.zeros(384),np.hanning(512)*2) #note the window, this is intentional
+plt.plot(data[128:rate-384]) #note that the first segment will not overlap right because of tapering oscillation
+plt.plot(i[384:])
+plt.plot(data[128:rate-384][0:512])
+plt.plot(i[384:][0:512])
