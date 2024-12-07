ae32c90529b2172ab961b24749fa97813ec16eee
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2024-12-07 14:37:17 -0500

    Update Wolf.py

--- Wolf.py
+++ Wolf.py
@@ -144,14 +144,76 @@ class TigerOptimizer(Optimizer):
 
         return loss
 
-optimal learn rate is as follows
+import torch
+from torch.optim.optimizer import Optimizer
+class WolfLearnRate(Optimizer):
+  """Implements Wolf algorithm."""
 
-self.t = 0
+  def __init__(self, params, lr=0.25, betas=(0.9, 0.999), eps=1e-8):
+        # Define default parameters
+        defaults = dict(lr=lr, betas=betas, eps=eps)
+        self.lr = lr
+        self.t = 0
+        self.alpha=256 #not yet figured out
+        # Initialize the parent Optimizer class first
+        super().__init__(params, defaults)
+        # Constants specific to Wolf
+        # Initialize state for each parameter
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['p'] = torch.zeros_like(p)  # Second moment estimate
 
-in step:
-t+1(at END, not at beginning!)
+  @torch.no_grad()
+  def step(self, closure=None):
+    """Performs a single optimization step.
 
-lr = 2/(t + 2) - log(2/(t + 2) + 1)
-lr = 2/(t/alpha + 2) - log(2/(t/alpha + 2) + 1)
-alpha = 1 default behavior, <1 more aggressive >1 less aggressive
-suggest try set it to the number of parameters in the current layer
+    Args:
+      closure (callable, optional): A closure that reevaluates the model
+        and returns the loss.
+
+    Returns:
+      the loss.
+    """
+    etcerta = 0.367879441
+    et = 1 - etcerta
+
+    loss = None
+    if closure is not None:
+      with torch.enable_grad():
+        loss = closure()
+
+    for group in self.param_groups:
+      for p in group['params']:
+        if p.grad is None:
+          continue
+        state = self.state[p]
+            
+            # Update step count
+
+        
+        # Perform stepweight decay
+        grad = p.grad
+        state = self.state[p]
+        # State initialization
+
+        exp_avg = state['p'] 
+        # Weight update
+        update = exp_avg * et + grad * etcerta
+        state['p']  = exp_avg * et + update * etcerta
+        sign_agreement = torch.sign(update) * torch.sign(grad)
+        # The value to use for adaptive_alpha depends upon your model.
+        #in general, test and set it as high as you can without it exploding
+        #it may favor a backoff where it starts small and then gets larger as the model converges on the global minimum
+        print(p.data)
+        adaptive_alpha = self.lr
+        # Where signs agree (positive), apply normal update
+        mask = (sign_agreement > 0)
+        lr = 2/(self.t/self.alpha + 2) - math.log(2/(self.t/self.alpha + 2) + 1)
+
+        p.data = torch.where(mask, 
+                            p.data - lr * update,
+                            p.data)
+        p.grad.zero_()
+        self.t = self.t + 1
+    return loss
