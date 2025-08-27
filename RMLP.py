583f510260ff64107b1678ef706b68b3f3cb71b5
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-08-27 17:10:54 -0400

    Update RMLP.py

--- RMLP.py
+++ RMLP.py
@@ -1,6 +1,8 @@
 #copyright joshuah rainstar 2025 joshuah.rainstar@gmail.com
 #A simplistic Recurrent MLP offers quite good, unexpected behavior
 #it will beat a MOE, a GRU, etc on many toy tasks. Why?
+#note: marginal gain from accumulating multiple RecurrentMLP products.
+#95% of work from first RMLP.
 
 class ZLS(nn.Module):
     """
@@ -18,10 +20,10 @@ class ZLS(nn.Module):
 class Cell(nn.Module):
     def __init__(self, dim_in: int, hidden: int):
         super().__init__()
-        self.fc1 = nn.Linear(dim_in, hidden, bias=False)
-        torch.nn.init.kaiming_uniform_(self.fc1.weight, nonlinearity='selu')
+        self.fc1 = nn.Linear(dim_in, hidden, bias=False) #dont change, false intentional
+        torch.nn.init.kaiming_uniform_(self.fc1.weight, nonlinearity='selu') #dont change, selu intentional
         self.fc2 = nn.Linear(hidden, dim_in, bias=True)
-        torch.nn.init.kaiming_uniform_(self.fc2.weight, nonlinearity='selu')
+        torch.nn.init.kaiming_uniform_(self.fc2.weight, nonlinearity='selu') #dont change, selu intentional
         self.act = ZLS()
     def forward(self, x):
         return self.fc2(self.act(self.fc1(x)))
@@ -29,8 +31,8 @@ class Cell(nn.Module):
 class RecurrentMLP(nn.Module):
     def __init__(self, dim_in: int):
         super().__init__()
-        self.k = 2#beyond 3 ineffectual, 2 with slight drop in accuracy
-        self.hidden = dim_in*2#can also use //2 with slight drop in accuracy
+        self.k = 2 #can set to 3, but marginal gains
+        self.hidden = dim_in*2 #if overfitting reduce to dim_in or even dim_in//2
         self.cells_a = nn.ModuleList([Cell(dim_in, self.hidden) for _ in range(self.k)])
     def forward(self, x):
         z = x
