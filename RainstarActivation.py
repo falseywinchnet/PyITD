eb313277fe7d2fb0158649aa6ec54fc09e4bcbe4
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-08-27 14:48:18 -0400

    Update RainstarActivation.py

--- RainstarActivation.py
+++ RainstarActivation.py
@@ -12,6 +12,6 @@ class RainstarActivation(nn.Module):
     def __init__(self, gammaval=8):
         super().__init__()
     def forward(self, x):
-       neg =  nn.SiLU()(x) * (x*torch.sigmoid(x)) + x/(1+torch.abs(x))
+       neg =  (x*torch.sigmoid(x))**2 + x/(1+torch.abs(x))
        pos =  x -  x/(1+torch.abs(x))
        return (neg *torch.sigmoid(-x)) + (pos * torch.sigmoid(x))
