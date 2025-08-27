bf975e13fc53b4eee8aec37d930847531afcfe01
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-08-27 17:07:58 -0400

    Update RMLP.py

--- RMLP.py
+++ RMLP.py
@@ -2,6 +2,19 @@
 #A simplistic Recurrent MLP offers quite good, unexpected behavior
 #it will beat a MOE, a GRU, etc on many toy tasks. Why?
 
+class ZLS(nn.Module):
+    """
+    Zero-crossing Logistic-subtracted Softplus activation
+    """
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        sp = F.softplus(x)                    # numerically stable softplus
+        sa = torch.sigmoid(0.5 * x)             # s_a(x) with learned optimal shape param
+        ba = sa * (1.0 - sa)                  # s_a(x) * (1 - s_a(x))
+        return sp - 2.77258872223978123766 * ba #4-log(2) yields zero crossing behavior
+    
 class Cell(nn.Module):
     def __init__(self, dim_in: int, hidden: int):
         super().__init__()
@@ -9,14 +22,14 @@ class Cell(nn.Module):
         torch.nn.init.kaiming_uniform_(self.fc1.weight, nonlinearity='selu')
         self.fc2 = nn.Linear(hidden, dim_in, bias=True)
         torch.nn.init.kaiming_uniform_(self.fc2.weight, nonlinearity='selu')
-        self.act = nn.GELU()
+        self.act = ZLS()
     def forward(self, x):
         return self.fc2(self.act(self.fc1(x)))
 
 class RecurrentMLP(nn.Module):
     def __init__(self, dim_in: int):
         super().__init__()
-        self.k = 3#beyond 3 ineffectual, 2 with slight drop in accuracy
+        self.k = 2#beyond 3 ineffectual, 2 with slight drop in accuracy
         self.hidden = dim_in*2#can also use //2 with slight drop in accuracy
         self.cells_a = nn.ModuleList([Cell(dim_in, self.hidden) for _ in range(self.k)])
     def forward(self, x):
